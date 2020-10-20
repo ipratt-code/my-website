c2bf18365ab3ae573ac72c6ba876a23cc0b7a549
Ian Pratt <thingcoder1@gmail.com>
2020-10-20 22:58:42 +0000

    making the flask app

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template
+app = Flask('app')
 
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/aboutme/')
+def aboutme():
+    return render_template('aboutme.html')
+
+app.run(host='0.0.0.0', port=8080)
