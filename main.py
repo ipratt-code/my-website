d45faf339b9f56ff9e6bcd5fba0e7280fef8e64e
Ian Pratt <thingcoder1@gmail.com>
2020-10-21 00:58:06 +0000

    made the website better

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from flask import Flask, render_template
 app = Flask('app')
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
+
+@app.route('/aboutme')
+def aboutme():
+    return render_template('aboutme.html')
+
+@app.route('/otherwebsites')
+def otherwebsites():
+    return render_template('otherwebsites.html')
 
 app.run(host='0.0.0.0', port=8080, debug=True)
